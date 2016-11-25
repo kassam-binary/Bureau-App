623a6a745f1cb72b8caad983959c1ebf027ee935
kassam-binary <mustaphakassam@gmail.com>
2016-11-25 20:08:49 +0300

    Updating rates

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,52 @@
-console.log("kassam");
+$(document).ready(function(){
+    
+    $("#base_currncInpt").on("focus", function(){
+       //console.log("yap")
+       $("#dropdown_current").css("display","block")
+    });  
+    
+    is_current_opened = false;
+    $("#base_currncInpt").on("blur", function(){
+       console.log("yap")
+       is_current_opened = true;
+       //if(".dropdown-menu")
+       //$("#country_left:.dropdown-menu").le
+    });
+    
+    $("body:not(#dropdown_current)").on("click",function(){
+        console.log("outside");
+        if(is_current_opened){
+            is_current_opened = false;
+        $("#dropdown_current").hide();
+            
+        }else{
+            
+        }
+    });
+    
+    
+    $("#base_changing").on("focus", function(){
+       //console.log("yap")
+       $("#dropdown_change").css("display","block")
+    });
+    
+    is_change_opened = false;
+    $("#base_changing").on("blur", function(){
+       console.log("yap")
+       is_change_opened = true;
+       //if(".dropdown-menu")
+       //$("#country_left:.dropdown-menu").le
+    });
+    
+    $("body:not(#dropdown_change)").on("click",function(){
+        console.log("outside");
+        if(is_change_opened){
+            is_change_opened = false;
+        $("#dropdown_change").hide();
+            
+        }else{
+            
+        }
+    });
+    
+})
