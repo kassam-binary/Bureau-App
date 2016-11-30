d2812e48e40d9001b8f35a2550131952e5a553a3
kassam-binary <mustaphakassam@gmail.com>
2016-11-30 15:35:41 +0300

    Creating currency drop down list

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,58 @@
 $(document).ready(function(){
+    //Object data
+    var flags;
+    $.getJSON("json_files/flags.json",function(flagsData){
+        flags = flagsData;
+        //console.log(flags);
+        var v = "BR"
+        //country_flag(v);
+        //countries_flag()
+        currencyHave_list();
+        currencyChange_list();
+    });
+    
+    function country_flag(flag_name){
+        $.each(flags, function(i,flag){
+            //console.log(flag.currency_name);
+            if(flag.flag_name == flag_name){
+                console.log(flag.flag_name);
+                //return flag.flag_name;
+            };
+        });
+    };
+    
+    function countries_flag(){
+        $.each(flags, function(i,flag){
+            country_flag(flag.flag_name);
+        });
+    };
+    
+    function currencyHave_list(){
+        var drop_list = "";
+        drop_list += "<ul id=\'dropdown_current\' class=\'dropdown-menu\'>"
+            $.each(flags, function(i, value){
+            
+            drop_list +="<li>"+"<i class=\'flag flag-"+value.flag_name+"\'></i>"+"<span class=\'flag_name\'>"+value.currency_name+"</span>"+"</li>";
+            
+        })+ "</ul>";
+        
+        $("#currencyHave").append(drop_list);
+    };
+    //currencyChange_list()
+    
+    function currencyChange_list(){
+        //var countryFlag = country_flag(flag_name)
+        //var currencyName = "Tanzania shilingi";
+        var drop_list = "";
+        drop_list += "<ul id=\'dropdown_change\' class=\'dropdown-menu\'>"
+            $.each(flags, function(i, value){
+            
+            drop_list += "<li>"+"<i class=\'flag flag-"+value.flag_name+"\'></i>"+"<span class=\'flag_name\'>"+value.currency_name+"</span>"+"</li>";
+            console.log(i+" " +value.flag_name+" "+ value.currency_name)
+        }) + "</ul>";
+        
+        $("#currencyChange").append(drop_list);
+    };
     
     $("#base_currncInpt").on("focus", function(){
        //console.log("yap")
